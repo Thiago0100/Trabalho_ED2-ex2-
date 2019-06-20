0e26650b73e1cc2d93801c3acc23af30cce7c65b
jony5ds <37637355+jony5ds@users.noreply.github.com>
2019-06-20 19:53:10 -0300

    Corrigindo Bug
    Corrigi o bug, mas estou com um problema de encontrar o minimo na linha de cada matriz

--- TrabalhoED2_AV2(ex2)/grafo.c
+++ TrabalhoED2_AV2(ex2)/grafo.c
@@ -17,7 +17,7 @@ void cria_grafo(Grafo* g, int vertices)
     g->peso = INFINIT;
 
      for (i = 0; i < g->total_vertices; i++)
-        for (j = 0; i < g->total_vertices; i++)
+        for (j = 0; j < g->total_vertices; j++)
              g->mat[i][j] = INFINIT;
 
 
@@ -45,10 +45,11 @@ void melhor_caminho(Grafo *g)
 
     for (i = 0; i < g->total_vertices; i++)
     {
-        for (j = 0; i < g->total_vertices; i++)
+        for (j = 0; j < g->total_vertices; j++)
         {
             if(g->mat[i][j] != INFINIT && g->mat[i][j] < rota)
             {
+                rota = g->mat[i][j];
                 printf ("\nCaminho[%d][%d] peso %d\n", i, j,g->mat[i][j]);
 
             }
@@ -61,12 +62,12 @@ void imprime_grafo(Grafo *g)
 {
     int i,j;
     for (i = 0; i < g->total_vertices; i++)
-        for (j = 0; i < g->total_vertices; i++)
+        for (j = 0; j < g->total_vertices; j++)
         {
-            //if(g->mat[i][j] != INFINIT)
-            //{
+            if(g->mat[i][j] != INFINIT)
+            {
                 printf ("\nCaminho[%d][%d] = %d\n", i, j,g->mat[i][j]);
-            //}
+            }
         }
 
 }
--- TrabalhoED2_AV2(ex2)/main.c
+++ TrabalhoED2_AV2(ex2)/main.c
@@ -6,13 +6,13 @@
 
 int main()
 {
-  Grafo **g;
-  cria_grafo(&g, 6);
+  Grafo g;
+  cria_grafo(&g,4);
   adiciona_aresta(&g,0,1,5);
   adiciona_aresta(&g,0,3,2);
-  adiciona_aresta(&g,3,1,2);
-  adiciona_aresta(&g,3,2,8);
   adiciona_aresta(&g,1,2,2);
-  imprime_grafo(&g);
+  adiciona_aresta(&g,3,2,8);
+
+ melhor_caminho(&g);
 
 }
